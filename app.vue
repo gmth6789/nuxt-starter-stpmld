fae437e95545f77df94ec80520297e7774cf391c
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-22 12:21:59 +0700

    Initial commit

---
+++ app.vue
@@ -0,0 +1,142 @@
+<template>
+	<view class="container container24325">
+	<div class="diygw-title diygw-top pink">
+					<i onclick="location.href='index.html'" class="title-bar-icon diy-icon-home margin-left"></i>
+					<div class="title flex1 text-center textindent30">หน้าหลัก</div>
+				</div>
+	<div class="flex diygw-col-24 diygw-bottom">
+					<div class="diygw-grid diygw-actions">
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/global/grid1.png" />
+								</div>
+								<div class="diygw-grid-title">หน้าหลัก</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/global/grid2.png" />
+								</div>
+								<div class="diygw-grid-title">ภารกิจ</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/global/grid3.png" />
+								</div>
+								<div class="diygw-grid-title">อันดับ</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/global/grid5.png" />
+								</div>
+								<div class="diygw-grid-title">แชท</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/global/grid4.png" />
+								</div>
+								<div class="diygw-grid-title">ข้อมูล</div>
+							</div>
+						</button>
+					</div>
+				</div>
+	<div class="flex flex-wrap diygw-col-0 items-stretch flex9-clz">
+					<div class="diygw-col-0 text3-clz">กิจกรรมแชร์</div>
+					<img src="./static/sd.png" class="image-size diygw-image diygw-col-0 image-clz" mode="heightFix" />
+				</div>
+				<div class="flex flex-wrap diygw-col-24 justify-center items-end">
+					<div class="flex flex-wrap diygw-col-0 flex-direction-column flex8-clz">
+						<div class="diygw-col-0 text6-clz"></div>
+						<div class="diygw-col-0">แชร์ให้เพื่อน</div>
+					</div>
+				</div>
+				<div class="version text-grey flex-direction-column flex diygw-col-24 justify-center align-center">
+					
+					<div>http://www.diygw.com</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+</div>
+</template>
+
+
+
+<style lang="scss">
+
+@import '/css/index.css';
+
+	.flex9-clz {
+		flex-shrink: 0;
+		height: 32px !important;
+	}
+	.text3-clz {
+		padding-top: 5px;
+		border-bottom-left-radius: 3px;
+		overflow: hidden;
+		color: #ffffff;
+		padding-left: 12px;
+		font-size: 14px !important;
+		padding-bottom: 5px;
+		border-top-left-radius: 3px;
+		border-top-right-radius: 0px;
+		border-bottom-right-radius: 8px;
+		background-image: linear-gradient(45deg, #f43f3b 1%, #ec008c 99%);
+		padding-right: 12px;
+	}
+	.image-clz {
+		margin-left: -6px;
+		margin-top: 0px;
+		margin-bottom: 0px;
+		margin-right: 0px;
+	}
+	.image-size {
+		height: 32px !important;
+		width: 17px !important;
+	}
+	.flex8-clz {
+		background-color: #07c160;
+		padding-top: 8px;
+		color: #ffffff;
+		font-weight: bold;
+		padding-left: 30px;
+		font-size: 14px !important;
+		padding-bottom: 8px;
+		padding-right: 30px;
+	}
+	.text6-clz {
+		padding-top: 8px;
+		border-right: 3px solid #ffffff;
+		color: #ffffff;
+		font-weight: bold;
+		padding-left: 8px;
+		font-size: 14px !important;
+		padding-bottom: 8px;
+		border-bottom: 3px solid #ffffff;
+		background-color: #07c160;
+		flex-shrink: 0;
+		top: 0px;
+		left: 0px;
+		width: 6px !important;
+		position: absolute;
+		height: 6px !important;
+		padding-right: 8px;
+	}
+	.container24325 {
+		padding-left: 0px;
+		padding-right: 0px;
+
+		background-image: linear-gradient(0deg, #ffffff 0%, #ffffff 50%, #e3e3e3 100%);
+	}
+	.container24325 {
+		padding-bottom: 80px;
+	}
+</style>
